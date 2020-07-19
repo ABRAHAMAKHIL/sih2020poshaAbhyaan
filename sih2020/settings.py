40bf8231275e6b82b17cfd03e64002b4c7ec76f0
Akhil.R.Abraham <akhilabraham34@gmail.com>
2020-07-19 21:02:09 +0530

    update

--- sih2020/settings.py
+++ sih2020/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'sih2020.wsgi.application'
 DATABASES = {
 'default': {
 'ENGINE': 'django.db.backends.mysql',
-'NAME': 'augustPoshan',
+'NAME': 'sih2020',
 'USER': 'root',
 'PASSWORD': "",
 'HOST' : "127.0.0.1",
