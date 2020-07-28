9dcb733cd979c4e98f6e79670a307b90a7423441
Akhil.R.Abraham <akhilabraham34@gmail.com>
2020-07-28 15:18:32 +0530

    removed django filters

--- sih2020/settings.py
+++ sih2020/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django_filters',
     'django.contrib.staticfiles',
     'Accounts',
     'beneficiary',
