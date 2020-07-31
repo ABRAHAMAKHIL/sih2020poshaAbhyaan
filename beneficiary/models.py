2236457bf9e1f8dd8cff28303a2c790a33d3c106
Akhil.R.Abraham <akhilabraham34@gmail.com>
2020-07-31 12:01:42 +0530

    State added

--- beneficiary/models.py
+++ beneficiary/models.py
@@ -5,6 +5,10 @@ from django.contrib.auth.models import User
 
 
 class beneficiary_register(models.Model):
+    CHOICES = (
+        ("Andhra Pradesh","Andhra Pradesh"),("Arunachal Pradesh ","Arunachal Pradesh "),("Assam","Assam"),("Bihar","Bihar"),("Chhattisgarh","Chhattisgarh"),("Goa","Goa"),("Gujarat","Gujarat"),("Haryana","Haryana"),("Himachal Pradesh","Himachal Pradesh"),("Jammu and Kashmir ","Jammu and Kashmir "),("Jharkhand","Jharkhand"),("Karnataka","Karnataka"),("Kerala","Kerala"),("Madhya Pradesh","Madhya Pradesh"),("Maharashtra","Maharashtra"),("Manipur","Manipur"),("Meghalaya","Meghalaya"),("Mizoram","Mizoram"),("Nagaland","Nagaland"),("Odisha","Odisha"),("Punjab","Punjab"),("Rajasthan","Rajasthan"),("Sikkim","Sikkim"),("Tamil Nadu","Tamil Nadu"),("Telangana","Telangana"),("Tripura","Tripura"),("Uttar Pradesh","Uttar Pradesh"),("Uttarakhand","Uttarakhand"),("West Bengal","West Bengal"),("Andaman and Nicobar Islands","Andaman and Nicobar Islands"),("Chandigarh","Chandigarh"),("Dadra and Nagar Haveli","Dadra and Nagar Haveli"),("Daman and Diu","Daman and Diu"),("Lakshadweep","Lakshadweep"),("National Capital Territory of Delhi","National Capital Territory of Delhi"),("Puducherry","Puducherry")
+        
+        )
     u_user = models.OneToOneField(User,on_delete=models.CASCADE,default=None)
     u_fname = models.CharField(max_length=30,default=None)
     u_sname = models.CharField(max_length=30,default=None)
@@ -14,6 +18,7 @@ class beneficiary_register(models.Model):
     u_pincode = models.CharField(max_length=6,default=None)
     u_addr = models.CharField(max_length=200,default=None)
     u_district = models.CharField(max_length=20,default=None)
+    u_states = models.CharField(max_length=300, choices = CHOICES,null=True)
     u_phone = models.CharField(max_length=13,default=None)
     u_phno = models.CharField(max_length=13,default=None,null=True,blank=True,unique=True)
     u_type= models.BooleanField(null=True,blank=True,default=False)
